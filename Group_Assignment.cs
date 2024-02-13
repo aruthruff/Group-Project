274f19c99f0339d44bca1f19f1ba971277abb1c8
aruthruff <128638594+aruthruff@users.noreply.github.com>
2024-02-13 17:18:24 -0500

    Update Group_Assignment.cs

--- Group_Assignment.cs
+++ Group_Assignment.cs
@@ -20,31 +20,55 @@ namespace Group_Assignment
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
+
+        static void ProjectIntro() {
+            
             WriteLine("Thanks for contributing to the computer club!");
             WriteLine("Price per case is $5");
             WriteLine("There is a 10% Student Government Association FEE (will be automatically deducted)\n");
 
-            double sgaFee = .1;
+        }
+        static void SalesProject()
+        {
+            double sgaFee = .1;                      //10% Student Government Association Fee
+            double fiveFee = 2.4;
 
-            WriteLine("Enter quantity of cases sold: ");
-            double caseQuantity = Convert.ToDouble(ReadLine());
+            WriteLine("Enter quantity of cases sold: (12 bars in a case) ");
+            double caseQuantity = Convert.ToDouble(ReadLine()); //Customer Input for quantity of granola bars sold
+            caseQuantity = caseQuantity * 12;
             WriteLine();
 
-            WriteLine("Enter cost per case: ");
-            double casePrice = Convert.ToDouble(ReadLine());
+            WriteLine("Enter cost per bar: (Currently selling for $1 per bar)");
+            double barPrice = Convert.ToDouble(ReadLine());    //Customer Input for cost of granola bars
             WriteLine();
 
-            WriteLine("Your total proceeds BEFORE 10% SGA fee is: {0:c}\n " , + casePrice *  caseQuantity);
-            double caseResult = casePrice * caseQuantity;
+            WriteLine("Your total earnings before expenses: \n{0:c}\n ", +barPrice * caseQuantity);     //Output for proceeds BEFORE SGA fee
+            double caseResult = barPrice * caseQuantity;                                                //Calculate factor for barPrice and caseQuantity
+            
+            WriteLine("Amount you paid to vendor: \n{0:c}\n ", + caseResult / fiveFee);
+            double caseExpense = caseResult / fiveFee;
+            WriteLine("Total proceeds after case expenses: \n{0:c}\n ", + caseResult - caseExpense);
 
-            WriteLine("10% SGA fee is {0:c}:\n ", + sgaFee * caseResult);
-            double proceedTotal = sgaFee * caseResult;
-            double realTotal = caseResult - proceedTotal;
-            WriteLine("Your total proceeds AFTER 10% SGA fee is: {0:c} ", + realTotal);
+            
+
+            WriteLine("10% SGA fee is: \n{0:c}\n ", +sgaFee * caseResult);                                 // 10% SGA fee dollar amount 
+            double proceedTotal = sgaFee * caseResult;                                                   //Calculate factor for sgaFee and caseResult
+            double realTotal = caseResult - proceedTotal;                                                //Calculate difference for caseResult and proceedTotal
+                           
+            WriteLine("Your grand total profits are: \n{0:c} ", realTotal - caseExpense);
 
             ReadKey();
+        }
+            
+        static void Main(string[] args)
+        {
+            
+            ProjectIntro();                    //Call ProjectIntro() Method
+            
+            SalesProject();                    //Call SalesProject() Method
+
+            ReadKey();
+
         }
     }
 }

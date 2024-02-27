22af1e2c51a4c2ffcfd366f5c595f29766637b68
aruthruff <128638594+aruthruff@users.noreply.github.com>
2024-02-26 19:53:02 -0500

    Week 5

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+
+namespace SwitchTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a character for the triangle:");
+                char character = Console.ReadKey().KeyChar;
+
+                Console.WriteLine("\nEnter the size of the peak for the triangle (max 10):");
+                string sizeInput = Console.ReadLine();
+
+                int size;
+                if (!int.TryParse(sizeInput, out size) || size <= 0 || size > 10)
+                {
+                    Console.WriteLine("Invalid input for size. Using default size 3 and default character '*'.");
+                    size = 3;
+                    character = '*';
+                }
+
+                PrintIsoscelesTriangle(character, size);
+
+                Console.WriteLine("\nDo you want to print another triangle? (y/n)");
+                char continueInput = Console.ReadKey().KeyChar;
+
+                if (continueInput != 'y' && continueInput != 'Y')
+                    break;
+
+                Console.Clear();
+            }
+        }
+
+        static void PrintIsoscelesTriangle(char character, int size)
+        {
+            for (int i = 1; i <= size; i++)
+            {
+                for (int j = 0; j < size - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 0; j < 2 * i - 1; j++)
+                {
+                    Console.Write(character);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+
+}
+
+    
+
+
+    
+
+       
+
+
+
+
+
+    

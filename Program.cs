5598bc58f1b0f7dd2864c60212e46a99d1d1e981
aruthruff <128638594+aruthruff@users.noreply.github.com>
2024-03-11 19:45:28 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,71 +1,96 @@
-﻿using System;
+﻿/* 
+ * Programmer: Anthony Ruthruff
+ *             Richard Carson
+ *             Nathan Wenson
+ * Date: 03/11/2024
+ * Class: CITP 180
+ * Purpose: Program with Motorway Class, data members, instance methods, and ToString method to return data members
+ * Assignment: Chapter 4 - Programming Excercise 3
+ */
+
+using System;
 using static System.Console;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Reflection;
+using System.Globalization;
 
-
-namespace SwitchTest
+namespace Week6_Group
 {
+    class Motorway
+    {
+        public string highwayName;  //List of data members
+        public string streetType;
+        public string direction;
+        public string surface;
+        public int numberOfLanes;
+        public string toll;
+        public string tollMaintain;
+
+        public Motorway(string highway)  //Instance method for highway name
+        {
+            highwayName = highway;
+            
+        }
+
+        public Motorway(string highway, string Toll) //Instance method for highway name and toll
+        { 
+            highwayName= highway;
+            toll = Toll;
+        }
+
+        public Motorway(string highway, int lanes) //Instance method for highway name and number of lanes
+        {
+            highwayName = highway;
+            numberOfLanes = lanes;
+            
+        }
+
+        public override string ToString() //ToString method to return data members and labels
+        {
+            return "Name of Motorway: " + highwayName  + "\nToll: " + toll + "\nNumber of Lanes: " + numberOfLanes + "\nStreet Type: " +streetType + "\nDirection: " 
+                + direction + "\nSurface Type: " + surface + "Maintained by: " + tollMaintain;
+        }
+
+
+    }
+  
     internal class Program
     {
         static void Main(string[] args)
         {
-            while (true)
-            {
-                Console.WriteLine("Enter a character for the triangle:");
-                char character = Console.ReadKey().KeyChar;
+           
 
-                Console.WriteLine("\nEnter the size of the peak for the triangle (max 10):");
-                string sizeInput = Console.ReadLine();
+            Motorway motorway = new Motorway("I-96");
+            WriteLine(motorway);
+            WriteLine("");
+            
+            Motorway toll1 = new Motorway("I-96", "Yes");
+            WriteLine(toll1);
+            WriteLine("");
+            
+            Motorway lanes1 = new Motorway("I-96", 5);
+            WriteLine(lanes1);
 
-                int size;
-                if (!int.TryParse(sizeInput, out size) || size <= 0 || size > 10)
-                {
-                    Console.WriteLine("Invalid input for size. Using default size 3 and default character '*'.");
-                    size = 3;
-                    character = '*';
-                }
 
-                PrintIsoscelesTriangle(character, size);
+            ReadKey();
 
-                Console.WriteLine("\nDo you want to print another triangle? (y/n)");
-                char continueInput = Console.ReadKey().KeyChar;
-
-                if (continueInput != 'y' && continueInput != 'Y')
-                    break;
-
-                Console.Clear();
-            }
         }
 
-        static void PrintIsoscelesTriangle(char character, int size)
-        {
-            for (int i = 1; i <= size; i++)
-            {
-                for (int j = 0; j < size - i; j++)
-                {
-                    Console.Write(" ");
-                }
-
-                for (int j = 0; j < 2 * i - 1; j++)
-                {
-                    Console.Write(character);
-                }
-
-                Console.WriteLine();
-            }
-        }
     }
 
 }
+            
 
-    
+            
+        
 
 
-    
+
+       
+        
+        
 
        
 
@@ -73,4 +98,4 @@ namespace SwitchTest
 
 
 
-    
+

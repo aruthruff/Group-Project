128483d52b36700d59be1a333771649bd54a5e9c
aruthruff <128638594+aruthruff@users.noreply.github.com>
2024-03-19 17:22:33 -0400

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week7_Group
+{
+    class Park
+    {
+        private string parkName;
+        private string parkLocation;
+        private string parkType;
+        private int parkFacilities;
+        private double parkFee;
+        private int employeeAmount;
+        private int annualvisitorAmount;
+        private double annualBudget;
+        
+
+        public Park(string name, string location, string type)
+        {
+            parkName = name;
+            parkLocation = location;
+            parkType = type;
+        }
+
+        public Park(string name, string location, int facility) 
+        {
+            parkName = name;
+            parkLocation = location;
+            parkFacilities = facility;
+        }
+
+        public Park(double budget, int visitors)
+        {
+            annualBudget = budget;
+            annualvisitorAmount = visitors;
+            
+        }
+
+        public Park(int visitors, double fee)
+        {
+            annualvisitorAmount = visitors;
+            parkFee = fee;
+            
+        }
+
+           
+
+        public Park(string name, string location, string type, int facility, double fee, int employees, int visitors, double budget)
+        {
+            parkName = name;
+            parkLocation = location;
+            parkType = type;
+            parkFacilities = facility;
+            parkFee = fee;
+            employeeAmount = employees;
+            annualvisitorAmount = visitors;
+            annualBudget = budget;
+        }
+
+        public override string ToString()
+        {
+            return "\nName of Park: " + parkName + "\nPark Location: " + parkLocation + "\nType of Park: " + parkType + "\nNumber of Facilities Available: " + parkFacilities
+                + "\nEntrance Fee: " + parkFee + "\nNumber of Employees: " + employeeAmount + "\nAnnual Number of Visitors: " + annualvisitorAmount
+                + "\nAnnual Budget: " + annualBudget;
+        }
+
+    }
+}
+
+
+        
+            
+            
+            
+            
+            
+        
+        
+
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿/* 
+ * Programmer: Anthony Ruthruff
+ *             Richard Carson
+ *             
+ * Date: 03/19/2024
+ * Class: CITP 180
+ * Purpose: 
+ * Assignment: Chapter 4 - Programming Excercise 7
+ */
+
+using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week7_Group
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Park park = new Park("Saugatuck State Park", "Holland, MI", "State Park");
+            WriteLine(park);
+
+            Park facilities = new Park("Saugatuck State Park", "Holland, MI", 5 );
+            WriteLine(facilities);
+
+            Park visitorCost = new Park();
+            WriteLine(visitorCost);
+
+            Park revenue = new Park();
+            WriteLine(revenue);
+
+            ReadKey();
+        }
+    }
+}

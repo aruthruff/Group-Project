ccc23dba53d8252b76ca99f71faef8be6aa1b824
aruthruff <128638594+aruthruff@users.noreply.github.com>
2024-03-21 10:26:49 -0400

    Update Week7_Park_Class.cs

--- Week7_Park_Class.cs
+++ Week7_Park_Class.cs
@@ -8,61 +8,71 @@ namespace Week7_Group
 {
     class Park
     {
-        private string parkName;
+        private string parkName;                   //Data Members
         private string parkLocation;
         private string parkType;
-        private int parkFacilities;
+        private string parkFacilities;
         private double parkFee;
         private int employeeAmount;
         private int annualvisitorAmount;
         private double annualBudget;
-        
 
-        public Park(string name, string location, string type)
+        public string GetParkType()              //Instance Method 1 (Textbook Reference)
         {
-            parkName = name;
-            parkLocation = location;
-            parkType = type;
-        }
+            WriteLine("Enter Park Name: ");
+            parkName = ReadLine();
 
-        public Park(string name, string location, int facility) 
-        {
-            parkName = name;
-            parkLocation = location;
-            parkFacilities = facility;
-        }
+            WriteLine("\nEnter Park Location: ");
+            parkLocation = ReadLine();
 
-        public Park(double budget, int visitors)
-        {
-            annualBudget = budget;
-            annualvisitorAmount = visitors;
-        }
-        
-        public Park(int visitors, double fee)
-        {
-            annualvisitorAmount = visitors;
-            parkFee = fee;
-        }
-            
-        public Park(string name, string location, string type, int facility, double fee, int employees, int visitors, double budget)
-        {
-            parkName = name;
-            parkLocation = location;
-            parkType = type;
-            parkFacilities = facility;
-            parkFee = fee;
-            employeeAmount = employees;
-            annualvisitorAmount = visitors;
-            annualBudget = budget;
+            WriteLine("\nEnter Type of Park: ");
+            parkType = ReadLine();
+
+            return $"Park Name: {parkName}\nLocation: {parkLocation}\nPark Type: {parkType}";
         }
 
-        public override string ToString()
+        public string GetParkInfo()             //Instance Method 2 (Textbook Reference)
+        {
+            WriteLine("\nEnter Park Name: ");
+            parkName = ReadLine();
+
+            WriteLine("\nEnter Park Location: ");
+            parkLocation = ReadLine();
+
+            WriteLine("\nEnter Park Facilities: ");
+            parkFacilities = ReadLine();
+
+            return $"Park Name: {parkName}\nLocation: {parkLocation}\nFacility Type: {parkFacilities}";
+        }
+
+        public double ComputeCostPerVisitor()           //Instance Method 3 (Textbook Reference)
+        {
+            WriteLine("\nEnter this years budget: ");
+            annualBudget = Convert.ToDouble(ReadLine());
+
+            WriteLine("\nEnter total mount of Annual Visitors: ");
+            annualvisitorAmount = Convert.ToInt32(ReadLine());
+
+            return annualBudget / annualvisitorAmount;
+        }
+
+        public double ComputeRevenueFromFees()            //Instance Method 4 (Textbook Reference)
+        {
+            WriteLine("\nEnter Park Fee per visitor: ");
+            parkFee = Convert.ToDouble(ReadLine());
+
+            WriteLine("\nEnter total mount of Annual Visitors: ");
+            annualvisitorAmount = Convert.ToInt32(ReadLine());
+
+            return annualvisitorAmount * parkFee;
+        }
+
+        public override string ToString()                //ToString method that returns all data members
         {
             return "\nName of Park: " + parkName + "\nPark Location: " + parkLocation + "\nType of Park: " + parkType + "\nNumber of Facilities Available: " + parkFacilities
                 + "\nEntrance Fee: " + parkFee + "\nNumber of Employees: " + employeeAmount + "\nAnnual Number of Visitors: " + annualvisitorAmount + "\nAnnual Cost Per Visitor: " 
                 + annualBudget / annualvisitorAmount + "\nAnnual Revenue: " + parkFee * annualvisitorAmount;
         }
-
     }
 }
 

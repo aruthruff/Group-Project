32573547f8397580a5a18da8c82822eb9272d0de
aruthruff <128638594+aruthruff@users.noreply.github.com>
2024-03-26 20:29:12 -0400

    Week8_Group.cs
    Hey guys just FYI this is not finished. I'm uploading what I have so far so you guys can use it or add to it if needed. I plan on finishing tomorrow, but I'd like to see what the group has as well. I've created and compiled the first two arrays, a third array, and created the message box for output.
    The last step is to store the product of the first two arrays into the third array.

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿/* 
+ * Programmer: Anthony Ruthruff
+ *             Richard Carson
+ *             Kenneth Thompson
+ *             Nathaniel Gebers
+ *             Nathan Wenson
+ * Date: 03/26/2024
+ * Class: CITP 180
+ * Purpose:  
+ * Assignment: Chapter 7 - Programming Excercise 3
+ */
+
+using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace Week8_Group
+{
+    internal class Program
+    {
+        
+       
+        
+        static void Main(string[] args)
+        {
+            string caption = "System.Array Methods Illustrated";
+            double [] firstArray = new double[2] {10.5, 27};
+            double [] secondArray = new double[3] {99.7, 10.9, 7};
+            double [] thirdArray = new double[firstArray.Length + secondArray.Length] ;
+            /*double a = firstArray[0];
+            double b = firstArray[1];
+            double c = secondArray[0];
+            double d = secondArray[1];
+            double e = secondArray[2];
+            double total = a * b * c * d * e;*/
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+               
+                WriteLine (firstArray[i] * secondArray[i] + " ");
+            }
+
+            //WriteLine(Convert.ToString(total));
+            string outputMsg = "";
+            outputMsg += "ThirdArray: ";
+           
+            foreach (double x in firstArray)
+            {
+                outputMsg += x + ", ";
+            }
+            foreach (double y in secondArray)
+            {
+                outputMsg += y + ", ";
+            }
+            /*foreach (double z in thirdArray)
+            {
+                outputMsg = z + ", ";
+            }*/
+            MessageBox.Show(outputMsg);
+            ReadKey();
+            
+            firstArray.CopyTo(thirdArray, 0);
+            secondArray.CopyTo(thirdArray, 0);
+        }
+    }
+}
+           
+
+            
+
+
+
+            
+
+
+           
+
+            
+
+
+
+            
+
